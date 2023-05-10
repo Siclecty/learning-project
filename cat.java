217faa9c5ec7780a6bbb111f58fc44281b4425ef
Siclecty <132353436+Siclecty@users.noreply.github.com>
2023-05-10 23:16:59 +0800

    Add files via upload

---
+++ cat.java
@@ -0,0 +1,22 @@
+package com.learning.Cat;
+
+public class cat {
+    public static void main(String[] args) {
+        String name1="贝妮";
+        String gender1="母";
+        String color1="白色";
+        String locale1="花园";
+        String name2="布布";
+        String gender2="公";
+        String color2="灰色";
+        String locale2="操场";
+        String name3="阿姆";
+        String gender3="公";
+        String color3="白色";
+        String locale3="公园";
+        System.out.println("昵称"+"\t"+"性别"+"\t"+"颜色"+"\t"+"出没地点");
+        System.out.println(name1+"\t"+gender1+"\t"+color1+"\t"+locale1);
+        System.out.println(name2+"\t"+gender2+"\t"+color2+"\t"+locale2);
+        System.out.println(name3+"\t"+gender3+"\t"+color3+"\t"+locale3);
+    }
+}
