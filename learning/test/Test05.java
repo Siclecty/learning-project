f23fe6a2764b0280be347bff98dc26e5486d9bfb
Siclecty <132353436+Siclecty@users.noreply.github.com>
2023-05-11 20:16:24 +0800

    Add files via upload

---
+++ learning/test/Test05.java
@@ -0,0 +1,18 @@
+package com.learning.test;
+
+public class Test05 {
+    public static void main(String[] args) {
+        int a = 10;
+        {
+            a = 20;
+            System.out.println(a);
+        }
+
+        {
+            int b = 20;
+            System.out.println(b);
+        }
+        int b = 30;
+        System.out.println(b);
+    }
+}
