8d93b3d0f2dcfbc5b3b0ad53f3a98554710341dd
Siclecty <132353436+Siclecty@users.noreply.github.com>
2023-05-12 21:05:11 +0800

    Add files via upload

---
+++ learning/practice-code/Test07.java
@@ -0,0 +1,18 @@
+package com.learning.test;
+
+public class Test07 {
+    public static void main(String[] args) {
+        String name="张三";
+        char sex='男';
+        int age=18;
+        String add="xx省xx市";
+        float card=55.55F;
+        System.out.println("*********************");
+        System.out.println("我的名字是："+name);
+        System.out.println("我的性别是："+sex);
+        System.out.println("我的年龄是："+age);
+        System.out.println("我的地址是："+add);
+        System.out.println("我的饭卡余额是:"+card+"元");
+        System.out.println("*********************");
+    }
+}
